722d574fc6a2f8198436f44f1ff42949feb7debb
Scott Windsor <swindsor@gamil.com>
2023-11-13 22:41:23 -0800

    extract media server

--- configuration/default.nix
+++ configuration/default.nix
@@ -10,6 +10,7 @@
     #<home-manager/nixos>
     #./home-manager.nix
     ./monitoring
+    ./media-server
   ];
 
   # Bootloader.
@@ -57,13 +58,6 @@
   services.xserver = {
     layout = "us";
     xkbVariant = "";
-  };
-
-  services.caddy = {
-    enable = true;
-    virtualHosts."nixos.local".extraConfig = ''
-      reverse_proxy http://localhost:8096
-    '';
   };
 
   # Enable CUPS to print documents.
@@ -118,91 +112,16 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
 
-  nixpkgs.overlays = [
-    (self: super: {
-      vlc = super.vlc.override {
-        libbluray = super.libbluray.override {
-          withAACS = true;
-          withBDplus = true;
-        };
-      };
-    })
-  ];
-
   environment.systemPackages = with pkgs; [
-    avahi
-    handbrake
-    ffmpeg
-    jellyfin
-    jellyfin-ffmpeg
     (retroarch.override {
       cores = with libretro; [ bsnes genesis-plus-gx snes9x beetle-psx-hw ];
     })
     qmk
     qmk-udev-rules
     vim
-    vlc
     wget
     unzip
   ];
-
-  services.jellyfin.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
-    };
-  };
-
-  services.samba-wsdd.enable =
-    true; # make shares visible for windows 10 clients
-
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = smbnix
-      netbios name = smbnix
-      security = user 
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      #hosts allow = 192.168.0. 127.0.0.1 localhost
-      hosts allow = 0.0.0.0/0
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
-      #      public = {
-      #      path = "/mnt/Shares/Public";
-      #      browseable = "yes";
-      #      "read only" = "no";
-      #      "guest ok" = "yes";
-      #      "create mask" = "0644";
-      #      "directory mask" = "0755";
-      #      "force user" = "username";
-      #      "force group" = "groupname";
-      #};
-      private = {
-        path = "/media";
-        "valid users" = "scott";
-        browseable = "no";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "jellyfin";
-        "force group" = "jellyfin";
-      };
-    };
-  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -223,7 +142,6 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
   networking.firewall.allowPing = true;
-  services.samba.openFirewall = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ configuration/media-server/default.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, ... }: {
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."nixos.local".extraConfig = ''
+      reverse_proxy http://localhost:8096
+    '';
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      vlc = super.vlc.override {
+        libbluray = super.libbluray.override {
+          withAACS = true;
+          withBDplus = true;
+        };
+      };
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [
+    handbrake
+    ffmpeg
+    jellyfin
+    jellyfin-ffmpeg
+    (retroarch.override {
+      cores = with libretro; [ bsnes genesis-plus-gx snes9x beetle-psx-hw ];
+    })
+    vlc
+  ];
+
+  services.jellyfin.enable = true;
+
+  services.samba-wsdd.enable =
+    true; # make shares visible for windows 10 clients
+
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user 
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      #hosts allow = 192.168.0. 127.0.0.1 localhost
+      hosts allow = 0.0.0.0/0
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      #      public = {
+      #      path = "/mnt/Shares/Public";
+      #      browseable = "yes";
+      #      "read only" = "no";
+      #      "guest ok" = "yes";
+      #      "create mask" = "0644";
+      #      "directory mask" = "0755";
+      #      "force user" = "username";
+      #      "force group" = "groupname";
+      #};
+      private = {
+        path = "/media";
+        "valid users" = "scott";
+        browseable = "no";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "jellyfin";
+        "force group" = "jellyfin";
+      };
+    };
+  };
+
+  services.samba.openFirewall = true;
+}
