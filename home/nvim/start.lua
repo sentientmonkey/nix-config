f98c93a3a968b944d2e9015bdd54c1167a3e81a9
Scott Windsor <swindsor@gmail.com>
2024-03-10 10:09:38 -0700

    enable nvim tree

--- home/nvim/start.lua
+++ home/nvim/start.lua
@@ -1,3 +1,13 @@
 vim.cmd[[colorscheme tokyonight-storm]]
 
 vim.cmd[[ let test#strategy = 'neovim' ]]
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
