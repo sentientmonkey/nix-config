ba3b18334e7ecac8f62b81b4a05b9e66639cb14e
Scott Windsor <swindsor@gmail.com>
2024-06-30 11:27:42 -0700

    flake update

--- configuration/default.nix
+++ configuration/default.nix
@@ -57,9 +57,9 @@
   services.xserver.videoDrivers = ["intel"];
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
@@ -163,6 +163,6 @@
     dedicatedServer.openFirewall =
       true; # Open ports in the firewall for Source Dedicated Server
   };
-  hardware.opengl.driSupport32Bit =
+  hardware.graphics.enable32Bit =
     true; # Enables support for 32bit libs that steam uses
 }
