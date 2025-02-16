d0b2fe3fb1ab18dd85e649f2601a854b2027bce4
Scott Windsor <swindsor@gmail.com>
2025-02-16 10:27:43 -0800

    set macos default preferences for os setup

--- macos/default.nix
+++ macos/default.nix
@@ -6,7 +6,7 @@
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
-  environment.darwinConfig = "$HOME/workspace/nix-config";
+  #environment.darwinConfig = "$HOME/workspace/nix-config";
 
   # to fix issue with using existing nix
   ids.gids.nixbld = 30000;
@@ -17,13 +17,34 @@
 
   # Set Git commit hash for darwin-version.
   #system.configurationRevision = self.rev or self.dirtyRev or null;
+  # The platform the configuration will be used on.
+  #nixpkgs.hostPlatform = "aarch64-darwin";
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 6;
+  system = {
+    stateVersion = 6;
 
-  # The platform the configuration will be used on.
-  #nixpkgs.hostPlatform = "aarch64-darwin";
+    defaults = {
+      dock = {
+        autohide = true;
+        orientation = "left";
+        show-process-indicators = true;
+        show-recents = false;
+        static-only = false;
+      };
+
+      finder = {
+        AppleShowAllExtensions = true;
+        ShowPathbar = true;
+        FXEnableExtensionChangeWarning = false;
+      };
+
+      NSGlobalDomain = { "com.apple.swipescrolldirection" = false; };
+
+    };
+    keyboard.remapCapsLockToControl = true;
+  };
 
   users.users.scott = {
     name = "scott";
